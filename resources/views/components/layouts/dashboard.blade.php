08b50f5844bdee248d0d3dcedebaaf963450fecb
hares <haresncc@gmail.com>
2026-08-08 11:59:35 +0300

    auto close sidebar

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -31,7 +31,7 @@
 </head>
 
 <body @class([
-    'hold-transition sidebar-mini sidebar-open',
+    'hold-transition sidebar-mini',
     'dark-mode' => Auth::user()->mode === 'Dark',
 ])>
     <div class="wrapper">
